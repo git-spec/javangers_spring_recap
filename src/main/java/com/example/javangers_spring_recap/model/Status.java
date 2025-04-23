51c276435978945e23f27fc562585eaab9e812bd
git-spec <inkyfischer@gmail.com>
2025-04-23 12:35:31 +0200

    feat: Add model Status and Task.

---
+++ src/main/java/com/example/javangers_spring_recap/model/Status.java
@@ -0,0 +1,7 @@
+package com.example.javangers_spring_recap.model;
+
+public enum Status {
+    TODO,
+    DOING,
+    DONE
+}
---
+++ src/main/java/com/example/javangers_spring_recap/model/Task.java
@@ -0,0 +1,6 @@
+package com.example.javangers_spring_recap.model;
+
+
+public record Task(String id, String description, Status status) {
+
+}
