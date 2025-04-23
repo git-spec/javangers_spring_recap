a08c41eb32616d00ef11efbb5a5c60fa265a0b71
git-spec <inkyfischer@gmail.com>
2025-04-23 15:47:19 +0200

    feat: Create TaskController and add getAllTasks.

---
+++ src/main/java/com/example/javangers_spring_recap/controller/TaskController.java
@@ -0,0 +1,26 @@
+package com.example.javangers_spring_recap.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.javangers_spring_recap.model.Task;
+import com.example.javangers_spring_recap.service.TaskService;
+
+
+@RestController
+@RequestMapping("/api")
+public class TaskController {
+    TaskService service;
+
+    public TaskController(TaskService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks() {
+        return service.getAllTasks();
+    }
+}
