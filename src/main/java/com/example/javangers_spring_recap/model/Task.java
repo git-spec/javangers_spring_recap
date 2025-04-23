69ed50e13f29325081de191367453a6d98595418
git-spec <inkyfischer@gmail.com>
2025-04-23 12:46:27 +0200

    feat: Add annotation Document to Task.

--- src/main/java/com/example/javangers_spring_recap/model/Task.java
+++ src/main/java/com/example/javangers_spring_recap/model/Task.java
@@ -1,6 +1,9 @@
 package com.example.javangers_spring_recap.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
 
+
+@Document("Tasks")
 public record Task(String id, String description, Status status) {
 
 }
