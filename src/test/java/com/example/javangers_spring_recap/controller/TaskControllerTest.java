9b063b1a496814bf51ed4ce64ecc337238724b28
git-spec <inkyfischer@gmail.com>
2025-04-23 18:59:26 +0200

    test: Create integration test TaskControllerTest.

---
+++ src/test/java/com/example/javangers_spring_recap/controller/TaskControllerTest.java
@@ -0,0 +1,46 @@
+package com.example.javangers_spring_recap.controller;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.example.javangers_spring_recap.model.Status;
+import com.example.javangers_spring_recap.model.Task;
+import com.example.javangers_spring_recap.repository.TaskRepo;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TaskControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TaskRepo repo;
+
+    @Test
+    void testGetAllTasks_shouldReturnListOfTasks_whenIsBeenCalled() throws Exception {
+        // GIVEN
+        Task task = new Task("1", "blabla", Status.TODO);
+        repo.save(task);
+        // WHEN
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/tasks"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().json(
+                """
+                    [
+                        {
+                            "id": "1",
+                            "description": "blabla",
+                            "status": "TODO"
+                        }
+                    ]        
+                """
+            ));
+    }
+}
