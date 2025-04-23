0a5772bc0154aedade5d5bb53c5c762d3010bb4d
git-spec <inkyfischer@gmail.com>
2025-04-23 11:38:22 +0200

    first commit

---
+++ src/main/java/com/example/javangers_spring_recap/JavangersSpringRecapApplication.java
@@ -0,0 +1,13 @@
+package com.example.javangers_spring_recap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavangersSpringRecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavangersSpringRecapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javangers_spring_recap/JavangersSpringRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javangers_spring_recap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavangersSpringRecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
