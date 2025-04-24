72f9bb78211781b3981f643acf9f4746e78038f2
git-spec <inkyfischer@gmail.com>
2025-04-24 14:11:16 +0200

    test: Create getTaskByID_shouldReturnTask_whenGetID in TaskServiceTest.

--- src/test/java/com/example/javangers_spring_recap/service/TaskServiceTest.java
+++ src/test/java/com/example/javangers_spring_recap/service/TaskServiceTest.java
@@ -1,12 +1,14 @@
 package com.example.javangers_spring_recap.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import com.example.javangers_spring_recap.dto.TaskDTO;
+import com.example.javangers_spring_recap.exception.TaskNotFoundException;
 import com.example.javangers_spring_recap.model.Status;
 import com.example.javangers_spring_recap.model.Task;
 import com.example.javangers_spring_recap.repository.TaskRepo;
@@ -18,6 +20,7 @@ public class TaskServiceTest {
     TaskService service = new TaskService(mockRepo, mockID);
     Task task = new Task("1", "blabla", Status.OPEN);
     TaskDTO taskDTO = new TaskDTO("blabla", Status.OPEN);
+    String id = "1";
 
     @Test
     void getAllTasks_shouldReturnListOfTasks_whenIsBeenCalled() {
@@ -28,6 +31,17 @@ public class TaskServiceTest {
         List<Task> actual = service.getAllTasks();
         // THEN
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void getTaskByID_shouldReturnTask_whenGetID() throws TaskNotFoundException {
+        // GIVEN
+        Mockito.when(mockID.createID()).thenReturn(id);
+        Mockito.when(mockRepo.findById(id)).thenReturn(Optional.of(task));
+        // WHEN
+        Task actual = service.getTaskByID(id);
+        // THEN
+        assertEquals(task, actual);
     }
 
     @Test
