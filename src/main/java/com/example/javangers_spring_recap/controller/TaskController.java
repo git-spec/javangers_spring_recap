4242302cf204173c500301e81269700d67220502
git-spec <inkyfischer@gmail.com>
2025-04-24 19:18:22 +0200

    feat: Create deleteTask in TaskController.

--- src/main/java/com/example/javangers_spring_recap/controller/TaskController.java
+++ src/main/java/com/example/javangers_spring_recap/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.example.javangers_spring_recap.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,5 +44,10 @@ public class TaskController {
     @PutMapping("/todo/{id}")
     public Task updateTask(@RequestBody Task updatedTask) throws TaskNotFoundException {
         return service.updateTask(updatedTask);
+    }
+
+    @DeleteMapping("/todo/{id}")
+    public Task deleteTask(@PathVariable String id) throws TaskNotFoundException {
+        return service.deleteTask(id);
     }
 }
