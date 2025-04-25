782b8adef8dfd2e647000c8c84ec22d1af4502e1
git-spec <inkyfischer@gmail.com>
2025-04-25 13:00:09 +0200

    test: Create getTaskByID_shouldReturnTask_whenCalledByID in TasControllerTest.

--- src/test/java/com/example/javangers_spring_recap/controller/TaskControllerTest.java
+++ src/test/java/com/example/javangers_spring_recap/controller/TaskControllerTest.java
@@ -1,19 +1,20 @@
 package com.example.javangers_spring_recap.controller;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.example.javangers_spring_recap.model.Status;
 import com.example.javangers_spring_recap.model.Task;
 import com.example.javangers_spring_recap.repository.TaskRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @SpringBootTest
@@ -27,6 +28,9 @@ public class TaskControllerTest {
 
     @Autowired
     private TaskRepo repo;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void getAllTasks_shouldReturnListOfTasks_whenIsBeenCalled() throws Exception {
@@ -46,6 +50,29 @@ public class TaskControllerTest {
                     ]        
                 """
             ));
+    }
+
+    // Tests on the one hand the retrieval of task and on the other the random id.
+    @Test
+    void getTaskByID_shouldReturnTask_whenCalledByID() throws Exception {
+        MvcResult postResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(
+            """
+            {
+                "description": "blabla",
+                "status": "OPEN"
+            }
+            """
+        ))
+        .andReturn();
+
+        String postResultString = postResult.getResponse().getContentAsString();
+        Task createTask = objectMapper.readValue(postResultString, Task.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/todo/" + createTask.id()))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().json(String.valueOf(objectMapper.writeValueAsString(createTask))));
     }
 
     @Test
