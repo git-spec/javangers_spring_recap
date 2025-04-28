ae1768025d75c5dbd26ddaad78831ce639c58cc7
git-spec <inkyfischer@gmail.com>
2025-04-28 17:18:36 +0200

    feat: Add filter readValue to Status.

--- src/main/java/com/example/javangers_spring_recap/model/Status.java
+++ src/main/java/com/example/javangers_spring_recap/model/Status.java
@@ -1,7 +1,27 @@
 package com.example.javangers_spring_recap.model;
 
+import com.example.javangers_spring_recap.exception.TaskNotFoundException;
+
+
 public enum Status {
-    OPEN,
-    IN_PROGRESS,
-    DONE
+    OPEN("open"),
+    IN_PROGRESS("in_progress"),
+    DONE("done");
+
+    private final String value;
+
+    Status(String value) {
+        this.value = value;
+    }
+
+    public static Status readValue(String value) throws TaskNotFoundException {
+        for (Status status: values()) {
+            if (status.value.equalsIgnoreCase(value)) {
+                return status;
+            } else {
+                throw new TaskNotFoundException("No enum constant " + value + '.');
+            }
+        }
+        return null;
+    }
 }
